c204714a19b448557e6ee2acf15c844574dc6e84
Lucas Emerick <lucasvemerick@gmail.com>
2025-04-29 20:04:08 -0300

    chore: remove comments

--- build.sh
+++ build.sh
@@ -6,10 +6,8 @@ ENTRY_POINT="src/main.cpp"
 BUILD_DIR="build"
 OUTPUT="$BUILD_DIR/$PROJECT_NAME"
 
-# Create build directory if it doesn't exist
 mkdir -p "$BUILD_DIR"
 
-# Compile using clang++ for the native M1 architecture
 clang++ "$ENTRY_POINT" -o "$OUTPUT"
 
 echo "Compilation done. Output: $OUTPUT"
